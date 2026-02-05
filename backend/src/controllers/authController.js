2c58ed31909c454e07d95decd8e79641c446bc89
Vansh111000 <vanshskataria@gmail.com>
2026-02-05 23:54:19 +0530

    Added Referesh token functionalities

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,8 +1,9 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
-
-
+import RefreshToken from "../models/RefreshToken.js";
+import ms from "ms";
+//The login will issue two tokens that is refresh and access
 
 export const register = async (req, res) => {
     const { name, email, phone, password } = req.body;
@@ -32,13 +33,14 @@ export const login = async (req, res) => {
     const password = req.body.password?.trim();
     const identifier = String(req.body.identifier).toLowerCase();
 
+
     // const user = users.find(i => ((i.email==identifier)||(i.phone==identifier)));
     const user = await User.findOne({
         $or: [
             { email: identifier },
             { phone: identifier }
         ]
-    }).select("+password");
+    });
     if (!user) {
         return res.status(400).json({ message: "Invalid Credentials " });
     }
@@ -55,5 +57,55 @@ export const login = async (req, res) => {
         { expiresIn: process.env.JWT_EXPIRES_IN }
     );
 
-    res.json({ token });
+    const refreshToken = jwt.sign(
+        { userId: user._id },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_REFRESH_TOKENS }
+    )
+
+    await RefreshToken.create({
+        user: user._id,
+        token: refreshToken,
+        expiresAt: new Date(Date.now() + ms(process.env.JWT_REFRESH_TOKENS))
+    });
+
+    res.json({ token, refreshToken });
 }
+
+//will generate new access token
+export const refresh = async (req, res) => {
+
+    const { refreshToken } = req.body;
+
+    if (!refreshToken) {
+        return res.status(401).json({ message: "Refresh token required " });
+    }
+
+    const storedToken = await RefreshToken.findOne({ token: refreshToken });
+
+    if (!storedToken) return res.status(403).json({ message: "Invalid refresh token" });
+
+    try {
+        const decoded = jwt.verify(refreshToken, process.env.JWT_SECRET);
+        const newAccessToken = jwt.sign(
+            {userId : decoded.userId},
+            process.env.JWT_SECRET,
+            {expiresIn : process.env.JWT_EXPIRES_IN}
+        );
+
+        res.json({token: newAccessToken});
+    } catch(e){
+        await RefreshToken.deleteOne({ token: refreshToken});
+        return res.status(403).json({message: "Refresh token expired"});
+    }
+
+};
+
+//logout
+export const logout = async (req,res)=>{
+    const { refreshToken }=req.body;
+    console.log("in logout ")
+    await RefreshToken.deleteOne({ token: refreshToken});
+
+    res.json({message: "Logged out successfully "});
+};
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,12 +1,19 @@
 import express from 'express';
-import {validationResult} from 'express-validator';
+
 import { loginValidator, registerValidator } from '../validators/authValidator.js';
 import validate from '../middlewares/validate.js';
-import { login, register } from '../controllers/authController.js';
+import { login, logout, refresh, register } from '../controllers/authController.js';
+
 
 const authRouter = express.Router();
 
 authRouter.post("/register", registerValidator, validate, register);
 authRouter.post("/login",loginValidator,validate,login);
+authRouter.post("/refresh-token", refresh);
+authRouter.post("/logout", logout)
+// authRouter.post("/logout", (req,res)=>{
+//     console.log("logout hit");
+//     res.status(201).json({message:"success"});
+// })
 
 export {authRouter};
